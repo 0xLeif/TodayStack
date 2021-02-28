aa0a854160cfdf4ca3c3382f055783f634618e1f
Zach Eriksen <zmeriksen@icloud.com>
2021-02-28 11:46:22 -0600

    Initial Commit

---
+++ Shared/TodayStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayStackApp.swift
+//  Shared
+//
+//  Created by Leif on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TodayStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
